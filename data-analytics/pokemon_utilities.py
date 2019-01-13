f8bbbc653a2de84a03e263e7be11a25455de5924
Bruno Roque <bruno.roque@spoud.io>
2019-01-14 00:24:06 +0100

    Include effective HP in the calculations

--- data-analytics/pokemon_utilities.py
+++ data-analytics/pokemon_utilities.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def effective_hp(base_hp):
+  return 31 + 2 * base_hp + 110
+
 def calculate_type_damage_factors(types_chart, attack_type, types1, types2):
   types1_factors = types_chart.loc[attack_type, types1].values
   types2_nulls = types2.isnull()
@@ -8,8 +11,13 @@ def calculate_type_damage_factors(types_chart, attack_type, types1, types2):
   types2_factors = np.where(types2_nulls, 1, types2_factors)
   return types1_factors * types2_factors
 
-def calculate_damages(defenses, type_factors):
+def calculate_damages(base_defenses, type_factors):
   level = 2 * 100 / 5 + 2
   power = 80
-  attack_defense = 70 / defenses
+  average_attack = effective_stat(80)
+  defenses = effective_stat(base_defenses)
+  attack_defense = average_attack / defenses
   return ((level * power * attack_defense) / 50 + 2) * type_factors
+
+def effective_stat(base_stat):
+  return 31 + 2 * base_stat + 5
