9242e0b8c2cc429083b7e46bb47d7699bf74a7c5
Bruno Roque <bruno.roque@spoud.io>
2019-01-13 21:30:04 +0100

    Fix second type damage factor

--- data-analytics/pokemon_utilities.py
+++ data-analytics/pokemon_utilities.py
@@ -1,7 +1,12 @@
+import numpy as np
+
 def calculate_type_damage_factors(types_chart, attack_type, types1, types2):
-  types1_factor = types_chart.loc[attack_type, types1].values
-  # type2_factor = types_chart.loc[attack_type, types2].values
-  return types1_factor
+  types1_factors = types_chart.loc[attack_type, types1].values
+  types2_nulls = types2.isnull()
+  types2_without_nulls = types2.replace(np.NaN, 'Normal')
+  types2_factors = types_chart.loc[attack_type, types2_without_nulls].values
+  types2_factors = np.where(types2_nulls, 1, types2_factors)
+  return types1_factors * types2_factors
 
 def calculate_damages(defenses, type_factors):
   level = 2 * 100 / 5 + 2
