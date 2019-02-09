5262794f57eab814fc5d5d1cbbd65742bdad17ea
Bruno Roque <bruno.roque@spoud.io>
2019-02-09 22:57:12 +0100

    Delete automated-mouse-keyboard-routine

--- automated-mouse-keyboard-routine/main.py
+++
@@ -1,7 +0,0 @@
-from src.AutomatedMouseKeyboardRoutine import AutomatedMouseKeyboardRoutine
-
-timeInSecondsBetweenEvents = 1
-numberOfTimesToRepeatRoutine = 2
-
-automatedGuiRoutine = AutomatedMouseKeyboardRoutine(timeInSecondsBetweenEvents, numberOfTimesToRepeatRoutine)
-automatedGuiRoutine.Process()
--- automated-mouse-keyboard-routine/src/Position.py
+++
@@ -1,4 +0,0 @@
-class Position(object):
-    def __init__(self, x: int, y: int) -> object:
-        self.x = x
-        self.y = y
