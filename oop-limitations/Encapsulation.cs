dc1488ff21d8e0eac452ef0b705312bfc3b6e3af
Bruno Roque <brunoroque06@gmail.com>
2020-03-07 00:26:36 +0100

    Add encapsulation

---
+++ oop-limitations/Encapsulation.cs
@@ -0,0 +1,17 @@
+namespace oop_limitations
+{
+    public class Kernel
+    {
+        public int Secret { get; set; }
+    }
+    
+    public class Shell
+    {
+        public Kernel Kernel { get; }
+
+        public Shell(Kernel kernel)
+        {
+            Kernel = kernel;
+        }
+    }
+}
---
+++ oop-limitations/EncapsulationTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace oop_limitations
+{
+    public class EncapsulationTests
+    {
+        [Test]
+        public void ShouldBeSafe()
+        {
+            var core = new Kernel();
+            core.Secret = 22;
+            
+            var shell = new Shell(core);
+            Assert.AreEqual(22, shell.Kernel.Secret, 0);
+
+            core.Secret = 18;
+            Assert.AreEqual(18, shell.Kernel.Secret, 0);
+        }
+    }
+}
