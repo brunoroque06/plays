a85078f504e69db4e64d8829289e954be06279fb
Bruno Roque <brunoroque06@gmail.com>
2020-03-10 23:43:24 +0100

    Add boiler plate for decision-tree-player nodejs version

---
+++ decision-tree-player/nodejs/index.js
@@ -0,0 +1,29 @@
+// Rules
+// - immutability
+// - declarative (no loops, etc)
+// - return objects from functions
+// - control exceptions by returning [error, result]
+
+// const req = name => {
+//   throw new Error(`The value ${name} is required.`);
+// };
+// const doStuff = ( stuff = req('stuff') ) => {
+//   ...
+// }
+
+const increasePrice = (item, increaseBy) => {
+  // never ever do this
+  item.price += increaseBy;
+
+  return item;
+};
+
+const oldItem = { price: 10 };
+const newItem = increasePrice(oldItem, 3);
+
+// prints newItem.price 13
+console.log("newItem.price", newItem.price);
+
+// prints oldItem.price 13
+// unexpected?
+console.log("oldItem.price", oldItem.price);
