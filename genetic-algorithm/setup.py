c53fc0dd686b2dcb796064b0f85b3249187ed355
Bruno Roque <brunoroque06@gmail.com>
2021-06-05 00:50:15 +0200

    Fix gen algo pipeline

---
+++ genetic-algorithm/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="genetic_algorithm", packages=find_packages())
---
+++ genetic-algorithm/test/stochastic/stochastic_test.py
@@ -0,0 +1,18 @@
+from genetic_algorithm import stochastic
+
+
+def test_integer():
+    num = stochastic.integer(10, 20)
+    assert 10 <= num <= 20
+
+
+def test_character():
+    char = stochastic.character("abc")
+    assert char in "abc"
+
+
+def test_string():
+    string = stochastic.string("abc", 10)
+    assert len(string) == 10
+    for char in string:
+        assert char in "abc"
