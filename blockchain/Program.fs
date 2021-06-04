db6e69ba66826ac1ee346bfb8136157586c552ac
Bruno Roque <brunoroque06@gmail.com>
2021-06-04 22:27:07 +0200

    Add dotnet format/lint

--- blockchain/Program.fs
+++ blockchain/Program.fs
@@ -3,11 +3,17 @@
 open System
 
 // Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let from whom = $"from %s{whom}\n"
+
+let m = from "Bruno"
+printf $"%s{m}"
+
+let sampleFunction1 x = x*x + 3
+let tuple1 = (1, 2, 3)
+printfn tuple1.[0]
 
 [<EntryPoint>]
 let main argv =
     let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    printfn $"Hello world %s{message}"
     0 // return an integer exit code
