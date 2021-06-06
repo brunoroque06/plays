5a81041604a936aa9644f5277121db1eed7e56ef
Bruno Roque <brunoroque06@gmail.com>
2021-06-06 20:12:48 +0200

    Add mating pool

--- genetic-algorithm/genetic_algorithm/individual.py
+++ genetic-algorithm/genetic_algorithm/individual.py
@@ -1,3 +1,4 @@
+import typing
 from dataclasses import dataclass
 
 from genetic_algorithm import genetics
@@ -7,3 +8,12 @@ from genetic_algorithm import genetics
 class Individual:
     genes: genetics.Genes
     fitness: float
+
+
+def mating_pool(fitnesses: typing.List[float]) -> typing.List[int]:
+    pool = []
+    fit_sum = sum(fitnesses)
+    for idx, fit in enumerate(fitnesses, start=0):
+        weight = int(fit / fit_sum * len(fitnesses) * 2)
+        pool = pool + [idx] * weight
+    return pool
--- genetic-algorithm/test/test_individual.py
+++ genetic-algorithm/test/test_individual.py
@@ -1,1 +1,6 @@
 from genetic_algorithm import individual
+
+
+def test_mating_pool():
+    pool = individual.mating_pool(fitnesses=[0.25, 0.5, 0.25, 0.75])
+    assert pool == [0, 1, 1, 2, 3, 3, 3]
