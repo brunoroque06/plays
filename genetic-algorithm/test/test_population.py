a730711d040e74af1701e1b592c541acf19245b1
Bruno Roque <brunoroque06@gmail.com>
2022-03-12 18:01:30 +0100

    Add test to find parents

--- genetic-algorithm/test/test_population.py
+++ genetic-algorithm/test/test_population.py
@@ -1,3 +1,4 @@
+from genetic_algorithm import individual
 from genetic_algorithm import population
 
 
@@ -14,4 +15,27 @@ def test_create_population():
         size=2,
     )
 
+    assert pop.generation == 0
     assert pop.max_fitness == 0.25
+    assert len(pop.individuals) == 2
+
+
+def test_find_parents():
+    par = 0
+
+    def random_int(_, __):
+        nonlocal par
+        par += 1
+        return 0 if par < 2 else 1
+
+    par_x, par_y = population.find_parents(
+        individuals=[
+            individual.Individual(fitness=0, genes="a"),
+            individual.Individual(fitness=1, genes="b"),
+        ],
+        pool=[0, 1, 2, 3],
+        random_int=random_int,
+    )
+
+    assert par_x == "a"
+    assert par_y == "b"
