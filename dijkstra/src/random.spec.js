e772348fa4d18120bae5abde31ab9fe9104630c4
Bruno Roque <brunoroque06@gmail.com>
2022-05-02 21:53:36 +0200

    Bump deps

--- dijkstra/src/random.spec.js
+++ dijkstra/src/random.spec.js
@@ -2,12 +2,14 @@ let random = require('./random');
 
 it('int', () => {
   const value = random.getInt(0, 10);
+
   expect(value).toBeGreaterThanOrEqual(0);
   expect(value).toBeLessThanOrEqual(10);
 });
 
 it('int multiple', () => {
   let values = random.getInts(0, 10, 5);
+
   expect(values.length).toBe(5);
   values.forEach((v) => {
     expect(v).toBeGreaterThanOrEqual(0);
