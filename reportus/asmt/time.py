37f79e74fdcf4fc20e6e231b7becc0377c4e3a52
Bruno Roque <brunoroque06@gmail.com>
2024-05-15 23:43:51 +0200

    Remove dead code

--- reportus/asmt/time.py
+++ reportus/asmt/time.py
@@ -1,14 +1,5 @@
 from datetime import date
 
-from dateutil.relativedelta import relativedelta
-
-
-# Required because pd.Interval does not support `relativedelta`, due to not being hashable
-def delta_idx(r: relativedelta, inc: bool = False) -> float:
-    d = 1 if inc else 0
-    r = (r + relativedelta(months=d)).normalized()
-    return float(f"{r.years}.{r.months:02}")
-
 
 def format_date(d: date, inc_day: bool = True) -> str:
     if inc_day:
